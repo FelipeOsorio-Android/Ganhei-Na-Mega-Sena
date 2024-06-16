d02e0da384d535e45e924243d1b5830d54f3bb0b
FelipeOsorio-Android <feeh_devandroid@hotmail.com>
2024-06-16 00:19:20 -0300

    add logic events

--- app/src/main/java/com/example/ganheinamegasena/MainActivity.kt
+++ app/src/main/java/com/example/ganheinamegasena/MainActivity.kt
@@ -1,20 +1,89 @@
 package com.example.ganheinamegasena
 
 import android.os.Bundle
-import androidx.activity.enableEdgeToEdge
+import android.view.View
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import androidx.core.widget.doOnTextChanged
+import androidx.recyclerview.widget.LinearLayoutManager
+import com.example.ganheinamegasena.adapter.NumbersBetsAdapter
+import com.example.ganheinamegasena.databinding.ActivityMainBinding
+import kotlin.random.Random
 
 class MainActivity : AppCompatActivity() {
+
+    private val binding by lazy {
+        ActivityMainBinding.inflate(layoutInflater)
+    }
+
+    private val adapter by lazy {
+        NumbersBetsAdapter()
+    }
+
+    private val editAmountBets by lazy {
+        binding.editAmountBets
+    }
+
+    private val editAmountNumbersBets by lazy {
+        binding.editAmountNumbersBets
+    }
+
+    private val btnGenerate by lazy {
+        binding.btnGenerate
+    }
+
+    private val txtAmountBets by lazy {
+        binding.txtAmountBets
+    }
+
+    private val txtAmountNumbersBets by lazy {
+        binding.txtAmountNumbersBets
+    }
+
+    private val recyclerNumbersBets by lazy {
+        binding.recyclerNumbersBets
+    }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
-        setContentView(R.layout.activity_main)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
-            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
-            insets
+        setContentView(binding.root)
+
+        editAmountNumbersBets.isEnabled = false
+        btnGenerate.isEnabled = false
+
+        //Layout
+        recyclerNumbersBets.layoutManager = LinearLayoutManager(applicationContext)
+
+        //Adapter
+        recyclerNumbersBets.adapter = adapter
+
+    }
+
+    override fun onResume() {
+        super.onResume()
+
+        editAmountBets.doOnTextChanged { text, start, before, count ->
+            text?.apply {
+                editAmountNumbersBets.isEnabled = this.isNotEmpty()
+            }
+        }
+
+        editAmountNumbersBets.doOnTextChanged { text, start, before, count ->
+            text?.apply {
+                btnGenerate.isEnabled = this.isNotEmpty() && this.toString().toInt() in 6..9
+            }
+        }
+
+        btnGenerate.setOnClickListener {
+            val amountBet = editAmountBets.text.toString().toInt()
+            val amountNumbers = editAmountNumbersBets.text.toString().toInt()
+
+            txtAmountBets.text = "Apostas: $amountBet"
+            txtAmountNumbersBets.text = "Jogo com: $amountNumbers numeros"
+
+            adapter.getList(amountBet, amountNumbers)
+            recyclerNumbersBets.visibility = View.VISIBLE
         }
     }
 }
